8add89ab321b38c7fd8a40d562d3f6c7a1889642
akshay shahane <akshayshahane@akshays-MacBook-Pro.local>
2020-05-30 18:40:54 +0530

    chore :  initial commit

---
+++ app/src/main/java/edu/self/thirty_days_of_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.self.thirty_days_of_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
