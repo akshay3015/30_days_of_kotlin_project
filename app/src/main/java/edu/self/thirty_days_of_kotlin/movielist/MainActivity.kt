45c0488d62e3569c5681b4ad59acacced0b17891
akshayshahane <akshay.shahane13@gmail.com>
2020-06-02 16:13:27 +0530

    feat : consumed top rated movies api

---
+++ app/src/main/java/edu/self/thirty_days_of_kotlin/movielist/MainActivity.kt
@@ -0,0 +1,82 @@
+package edu.self.thirty_days_of_kotlin.movielist
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.raywenderlich.wewatch.network.RetrofitClient
+import edu.self.thirty_days_of_kotlin.R
+import edu.self.thirty_days_of_kotlin.movielist.adapter.MovieListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val service = RetrofitClient.moviesApi
+        loaderVisibility(true)
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.getTopRatedMovies(RetrofitClient.API_KEY)
+            withContext(Dispatchers.Main) {
+                try {
+                    if (response.isSuccessful) {
+                        loaderVisibility(false)
+                        Log.d("res", response.body().toString())
+                        //Do something with response e.g show to the UI.
+                        val gridLayoutManager = GridLayoutManager(this@MainActivity, 3)
+                        rv_movies.layoutManager = gridLayoutManager
+                        val movielist = response.body()?.results
+                        Log.d("res", response.body()?.results.toString())
+                        rv_movies.adapter = MovieListAdapter(movieList = movielist)
+                        (rv_movies.adapter as MovieListAdapter).notifyDataSetChanged()
+
+                    } else {
+                        loaderVisibility(false)
+
+                        toast("Error: ${response.code()}")
+                    }
+                } catch (e: HttpException) {
+                    loaderVisibility(false)
+
+                    toast("Exception ${e.message}")
+                } catch (e: Throwable) {
+                    loaderVisibility(false)
+
+                    toast("Ooops: Something else went wrong")
+                }
+            }
+        }
+    }
+
+    private fun toast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG)
+
+    }
+
+
+    private fun loaderVisibility(b: Boolean) {
+        if (b) {
+            pb_loading_indicator.visibility = View.VISIBLE
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
+            tv_error_message_display.visibility = View.INVISIBLE
+        } else {
+            pb_loading_indicator.visibility = View.INVISIBLE
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            tv_error_message_display.visibility = View.VISIBLE
+        }
+    }
+}
---
+++ app/src/main/java/edu/self/thirty_days_of_kotlin/movielist/adapter/MovieListAdapter.kt
@@ -0,0 +1,40 @@
+package edu.self.thirty_days_of_kotlin.movielist.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.raywenderlich.wewatch.network.RetrofitClient
+import edu.self.thirty_days_of_kotlin.R
+import edu.self.thirty_days_of_kotlin.model.ResultsItem
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+class MovieListAdapter(var movieList: List<ResultsItem?>?) :
+    RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return if (movieList.isNullOrEmpty()) {
+            0
+        } else movieList!!.size
+    }
+
+    override fun onBindViewHolder(holder: MovieListAdapter.ViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(RetrofitClient.TMDB_IMAGEURL.plus(movieList?.get(position)?.posterPath))
+            .placeholder(R.drawable.ic_panorama_black_24dp)
+            .error(R.drawable.ic_error_outline_black_24dp)
+            .into(holder.ivMovie)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val ivMovie = view.movies_item
+    }
+
+}
